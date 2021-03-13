68702f8d219ab38dc7eda1b87e013aa9704392ca
radibaraq <radibaraq@gmail.com>
2021-03-13 17:37:51 +0200

    Initial Commit

---
+++ Shared/VoicesesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicesesApp.swift
+//  Shared
+//
+//  Created by Radi Barq on 13/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct VoicesesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
